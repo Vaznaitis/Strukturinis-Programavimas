c94716dee35fe9a51ecc877830fd96fc2cf5bac1
Tadas <vaznaitis8@gmail.com>
2024-09-12 14:57:43 +0300

    PirmaPrograma

---
+++ PirmaPrograma/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string vardas = "Tadas";
+    string pavarde = "Vaznaitis";
+    int amzius = 21;
+
+    cout << "Vardas: " << vardas << endl;
+    cout << "Pavarde: " << pavarde << endl;
+    cout << "Amzius: " << amzius << endl;
+
+    return 0;
+}
