c7c082f5f7a401aa0947c64f9da0bc4b4672027f
Tadas <vaznaitis8@gmail.com>
2024-09-12 15:22:06 +0300

    AntraPrograma

---
+++ AntraPrograma/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string Pavadinimas = "Kauno Zalgiris";
+    int Ikurimo_metai = 1944;
+    string Savininkas = "Kauno miesto savivaldybe";
+    string Arena = "Kauno Zalgirio arena";
+    int Vietos = 15000;
+
+    cout << Pavadinimas << endl;
+    cout << "Ikurimo_metai: " << Ikurimo_metai << endl;
+    cout << "Savininkas: " << Savininkas << endl;
+    cout << "Arena: " << Arena << endl;
+    cout << "Vietos:" << Vietos << endl;
+
+    return 0;
+}
---
+++ TreciaPrograma/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string marke = "Ford ";
+    string modelis = "Galaxy";
+    int metai = 2002;
+    int litrazas = 2.0;
+    string spalva = "Juodos";
+
+    cout << "Automobilis " << marke << modelis << " yra pagamintas " << metai << " metais. Jo motoras " << litrazas << " litrazo. Automobilis yra " << spalva << " Spalvos. " << endl;
+
+    return 0;
+}
