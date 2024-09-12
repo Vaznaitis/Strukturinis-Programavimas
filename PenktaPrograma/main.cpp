99321a5c570eb456fae3260511e28ceb8a9704be
Tadas <vaznaitis8@gmail.com>
2024-09-12 15:39:34 +0300

    SestaPrograma

---
+++ PenktaPrograma/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int e = 50;
+    int x;
+
+    x = (a + b + c + d + e) / 5;
+
+    cout << "Vidurkis: " << x << endl;
+
+    return 0;
+}
---
+++ SestaPrograma/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sk, a, b, c;
+    cin >> sk;
+    a = sk / 10;
+    b = sk % 10;
+    c = a + b;
+
+    cout << c << endl;
+
+    return 0;
+}
