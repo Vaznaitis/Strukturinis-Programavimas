5565dff826ae15800d629d0983d71f0731b44d63
Tadas <vaznaitis8@gmail.com>
2024-11-27 18:53:16 +0200

    asciiveikia

--- 3Praktine/main.cpp
+++ 3Praktine/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 const char ABECELE[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 string Uzsifravimas(char tekstas[],char raktas[]) {
+    char rezultatas[100];
     int indeksasTeksto=-1;
     int indeksasRakto=-1;
     int indeksasRezultato;
@@ -15,7 +16,6 @@ string Uzsifravimas(char tekstas[],char raktas[]) {
         raktasString.append(raktasString.begin(), raktasString.end());
     }
     strcpy(raktas, raktasString.c_str());
-    char rezultatas[100];
     for(int i = 0; i < tekstoIlgis; i++) {
 
         for(int x = 0; x < sizeof(ABECELE); x++) {
@@ -38,11 +38,11 @@ string Uzsifravimas(char tekstas[],char raktas[]) {
     string rezultatasString="";
     for (int i = 0; i < tekstoIlgis; i++) {
         rezultatasString=rezultatasString + rezultatas[i];
-    }
-
-    return rezultatasString;
+    }return rezultatasString;
 }
+
 string Desifravimas(char tekstas[],char raktas[]) {
+    char rezultatas[100];
     int indeksasTeksto=-1;
     int indeksasRakto=-1;
     int indeksasRezultato;
@@ -52,7 +52,6 @@ string Desifravimas(char tekstas[],char raktas[]) {
         raktasString.append(raktasString.begin(), raktasString.end());
     }
     strcpy(raktas, raktasString.c_str());
-    char rezultatas[100];
     for(int i = 0; i < tekstoIlgis; i++) {
 
         for(int x = 0; x < sizeof(ABECELE); x++) {
@@ -75,50 +74,101 @@ string Desifravimas(char tekstas[],char raktas[]) {
     string rezultatasString="";
     for (int i = 0; i < tekstoIlgis; i++) {
         rezultatasString=rezultatasString + rezultatas[i];
-    }
-
-    return rezultatasString;
+    }return rezultatasString;
 }
+string UzsifravimasASCII(char tekstas[],char raktas[]) {
+    char rezultatas[100];
+    int asciiPradzia=33,asciiPabaiga=126;
+    int asciiIlgis= asciiPabaiga-asciiPradzia+1;
+    int indeksasTeksto=-1;
+    int indeksasRakto=-1;
+    int tekstoIlgis = strlen(tekstas);
+    string raktasString = raktas;
+    while (raktasString.length() < strlen(tekstas)) {
+        raktasString.append(raktasString.begin(), raktasString.end());
+    }
+    strcpy(raktas, raktasString.c_str());
+    for(int i = 0; i < tekstoIlgis; i++) {
+
+        for(int x = asciiPradzia; x < asciiPabaiga; x++) {
+            if(tekstas[i] == static_cast<unsigned char>(x)) {
+                indeksasTeksto=x;
+            }
+            if(raktas[i] == static_cast<unsigned char>(x)) {
+                indeksasRakto=x;
+            }
+            if(indeksasTeksto != -1 && indeksasRakto != -1) {
+                int indeksasRezultato = ((indeksasTeksto - asciiPradzia) + (indeksasRakto - asciiPradzia)) % asciiIlgis +
+                                        asciiPradzia;
+
+                rezultatas[i] = (unsigned char)indeksasRezultato;
+                indeksasTeksto=-1;
+                indeksasRakto=-1;
+            }
+
+        }
+
+    }
+    string rezultatasString="";
+    for (int i = 0; i < tekstoIlgis; i++) {
+        rezultatasString=rezultatasString + rezultatas[i];
+    }return rezultatasString;
+}
+
 int main() {
     char tekstas[100];
     char raktas[100];
+    char ascTekstas[100];
+    char ascRaktas[100];
     string rezultatas;
     int pasirinkimas;
-    cout << "Pasirinkite sifravimo buda:" << endl;
-    cout << "1. Naudojant abecele" << endl;
-    cout << "2. Naudojant ASCII koduote" << endl;
-    cout << "Iveskite savo pasirinkima:" << endl;
-    cin >> pasirinkimas;
-    if (pasirinkimas != 3) {
-        cout << "Pasirinkite kokia operacija norite atlikti:" << endl;
-        cout << "1. Sifruoti/Desifruoti naudojant abecele" << endl;
-        cout << "2. Sifruoti/Desifruoti naudojant ASCII koduote" << endl;
-        cout << "3. Iseiti" << endl;
-        cout << "Iveskite savo pasirinkima:" << endl;
+        while (pasirinkimas != 3) {
+        cout <<"Pasirinkite operacija kuria norite atlikti su tekstu"<<endl;
+        cout <<"1. Sifravimas/Desifravimas naudojant abecele"<<endl;
+        cout <<"2. Sifravimas/Desifravimas naudojant ASCII koduote"<<endl;
+        cout <<"3. Iseiti"<<endl;
+        cout <<"Jusu pasirinkimas:"<<endl;
         cin >> pasirinkimas;
-    }
-    if (pasirinkimas == 1) {
-        int pasirinkimas2;
-        cout << "Pasirinkite kokia operacija norite atlikti:" << endl;
-        cout << "1. Sifruoti naudojant abecele" << endl;
-        cout << "2. Desifruoti naudojant abecele" << endl;
-        cout << "Iveskite savo pasirinkima:" << endl;
-        cin >> pasirinkimas2;
-        if (pasirinkimas2 == 1) {
-            cout << "Iveskite teksta kuri norite uzsifruoti" << endl;
-            cin>>tekstas;
-            cout << "Iveskite rakta kuri naudosite" << endl;
-            cin>>raktas;
-            rezultatas = Uzsifravimas(tekstas, raktas);
-            cout << "Uzsifruotas tekstas: "<< rezultatas << endl;
+
+        if (pasirinkimas==1) {
+            int pasirinkti;
+            cout <<"Pasirinkite operacija kuria norite atlikti:"<<endl;
+            cout <<"1. Sifravimas naudojant abecele"<<endl;
+            cout <<"2. Desifravimas naudojant abecele"<<endl;
+            cin>>pasirinkti;
+            if(pasirinkti==1) {
+                cout <<"Irasykite teksta kuri norite uzsifruoti:"<<endl;
+                cin>>tekstas;
+                cout <<"Irasykite rakta kuri norite naudoti:"<<endl;
+                cin>>raktas;
+                rezultatas = Uzsifravimas(tekstas, raktas);
+                cout << "Uzsifruotas tekstas: "<< rezultatas << endl;
+            }else if (pasirinkti==2) {
+                cout <<"Irasykite teksta kuri norite desifruoti:"<<endl;
+                cin>>tekstas;
+                cout <<"Irasykite rakta kuri norite naudoti:"<<endl;
+                cin>>raktas;
+                rezultatas = Desifravimas(tekstas, raktas);
+                cout << "Desifruotas tekstas: "<< rezultatas << endl;
+            }
+        } else if (pasirinkimas==2){
+            int pasirinkti;
+            cout <<"Pasirinkite operacija kuria norite atlikti:"<<endl;
+            cout <<"1. Sifravimas naudojant ASCII koduote"<<endl;
+            cout <<"2. Desifravimas naudojant ASCII koduote"<<endl;
+            cin>>pasirinkti;
+            if(pasirinkti==1) {
+                cout <<"Irasykite teksta kuri norite uzsifruoti:"<<endl;
+                cin>>ascTekstas;
+                cout <<"Irasykite rakta kuri norite naudoti:"<<endl;
+                cin>>ascRaktas;
+                rezultatas = UzsifravimasASCII(ascTekstas, ascRaktas);
+                cout << "Uzsifruotas tekstas: "<< rezultatas << endl;
+            }
+
+        } else if(pasirinkimas != 3){
+            cout << "Pasirinkimas neteisingas, bandykite dar karta"<<endl;
         }
-        else if (pasirinkimas2 == 2) {
-            cout <<"Iveskite teksta kuri norite desifruoti:"<<endl;
-            cin>>tekstas;
-            cout <<"Iveskite rakta kuri naudosite:"<<endl;
-            cin>>raktas;
-            rezultatas = Desifravimas(tekstas, raktas);
-            cout << "Desifruotas tekstas: "<< rezultatas << endl;
-        }
     }
-return 0;}
+    return 0;
+}
